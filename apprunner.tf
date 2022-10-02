00ddb03051ac0e594c058549983a858c949ed12d
glennbech <glenn.bech@gmail.com>
2022-10-02 20:27:08 +0200

    initial commit

--- apprunner.tf
+++ apprunner.tf
@@ -1,26 +1,43 @@
-resource "aws_s3_bucket" "frontend" {
-  bucket =  "glenn-demobucket"
-  acl = "public-read"
-  policy = <<EOF
-{
-  "Id": "bucket_policy_site",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "bucket_policy_site_main",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::glenn-demobucket/*",
-      "Principal": "*"
+resource "aws_apprunner_service" "example" {
+  service_name = "example"
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner-service-role.arn
     }
-  ]
-}
-EOF
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+      image_identifier      = var.image
+      image_repository_type = "ECR_PRIVATE"
+    }
+    auto_deployments_enabled = true
+  }
+  tags = {
+    Name = "example-apprunner-service"
   }
 }
 
+resource "aws_iam_role" "apprunner-service-role" {
+  name               = "${var.prefix}-AppRunnerECRAccessRole"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.apprunner-service-assume-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner-service-role-attachment" {
+  role       = aws_iam_role.apprunner-service-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+data "aws_iam_policy_document" "apprunner-service-assume-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["build.apprunner.amazonaws.com"]
+    }
+  }
+}
