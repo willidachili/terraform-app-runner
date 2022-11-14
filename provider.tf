273fd7616beaee68c4ff4f2f9e561a10f322644c
glennbech <glenn.bech@gmail.com>
2022-11-14 21:56:36 +0100

    remove if

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "<student-navn>/apprunner-actions.state"
+    key    = "glenn.richard.bech/apprunner.state"
     region = "eu-north-1"
   }
 }
