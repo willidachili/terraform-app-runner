aaf8038512bff63191d3cb5f5c0c11386e625269
willidachili <groundon87@hotmail.com>
2022-11-15 11:14:28 +0000

    did some stuff

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "glenn.richard.bech/apprunner-a-new-state.state"
+    key    = "wine001/apprunner-wine001-new.state"
     region = "eu-north-1"
   }
 }
